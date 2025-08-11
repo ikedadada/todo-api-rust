e33570d1d9a11a2c275253d1ac1ad86cc823b351
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-08-11 23:27:15 +0900

    feat:axumによるサンプルAPIの作成まで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, routing::get, serve};
+
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
+
+#[derive(Deserialize, Serialize)]
+struct Hello {
+    message: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/hello",
+            get(|| async { "Hello from /hello!" }).post(|Json(input): Json<Hello>| async move {
+                format!("Hello from /hello! You sent: {}", input.message)
+            }),
+        );
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+
+    serve(listener, app).await.unwrap();
 }
